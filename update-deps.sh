aa0ee038dfe8717c64db902feaf5d7cd0acb9d68
Shane R. Spencer <305301+whardier@users.noreply.github.com>
2020-07-14 22:31:23 +0000

    init

--- update-deps.sh
+++ update-deps.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+poetry add any-case@latest
 poetry add dpkt@latest
 poetry add humanize@latest
 poetry add inflect@latest
 poetry add pydantic@latest -E email -E dotenv
+poetry add stevedore@latest
 poetry add structlog@latest
+poetry add toml@latest
 poetry add typer@latest -E all
 poetry add ursine@latest
 
